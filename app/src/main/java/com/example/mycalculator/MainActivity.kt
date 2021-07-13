dbf65cdc02fd018c729db149dd46673cf4d5b023
daniel shelton <danielshelton90@gmail.com>
2021-07-13 09:11:46 -0700

    created onDecimalPoint()

--- app/src/main/java/com/example/mycalculator/MainActivity.kt
+++ app/src/main/java/com/example/mycalculator/MainActivity.kt
@@ -8,6 +8,10 @@ import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    var lastNumeric : Boolean = false
+    var lastDot : Boolean = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -15,10 +19,21 @@ class MainActivity : AppCompatActivity() {
 
     fun onDigit(view: View) {
         tvInput.append((view as Button).text)
-        Toast.makeText(this, "Button works", Toast.LENGTH_SHORT).show()
+//        Toast.makeText(this, "Button works", Toast.LENGTH_SHORT).show()
+        lastNumeric = true
     }
 
     fun onClear(view: View) {
         tvInput.text = ""
+        lastNumeric = false
+        lastDot = false
+    }
+
+    fun onDecimalPoint(view: View) {
+        if (lastNumeric && !lastDot) {
+            tvInput.append(".")
+            lastNumeric = false
+            lastDot = true
+        }
     }
 }
